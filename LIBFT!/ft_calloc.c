8970dd67d6acd73e3ae13785e8de71caab2d0836
bramijer <brami.jer@gmail.com>
2022-04-03 23:57:21 +0200

    02

--- LIBFT!/ft_calloc.c
+++ LIBFT!/ft_calloc.c
@@ -6,12 +6,12 @@
 /*   By: jbrami <jbrami@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/02 22:43:06 by jbrami            #+#    #+#             */
-/*   Updated: 2022/04/02 22:50:58 by jbrami           ###   ########.fr       */
+/*   Updated: 2022/04/03 21:37:38 by jbrami           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 /* The calloc function allocate memory. The allocated memory is aligned such that 
-it can be used for any data type. The free fucntion frees allocations that were
+it can be used for any data type. The free function frees allocations that were
 created via the preceding allocation functions.
 RETURN VALUES : If successfull calloc function return a pointer to allocate memory. 
 If there is an error, they return a NULL pointer. The free function does not return a value. */
@@ -19,12 +19,40 @@ If there is an error, they return a NULL pointer. The free function does not ret
 #include <stdio.h>
 #include <stdlib.h>
 
-void    *ft_calloc(size_t count, size_t size)
+void	ft_bzero(void *s, size_t n)
 {
-    
+	unsigned char	*ptr;
+
+	ptr = (unsigned char *)s;
+	while (n > 0)
+	{
+		*ptr++ = 0;
+		n--;
+	}
 }
 
-int main 
+void    *ft_calloc(size_t count, size_t size)
 {
+    void    *memory;
     
+    memory = NULL;
+    if (count && (size_t) - 1 / count < size)
+    {
+        return (NULL);   
+    }
+    memory = malloc (count * size);
+    if (!memory)
+    {
+        return (NULL);
+    }
+    ft_bzero(memory, count * size);
+    {
+        return (memory);
+    }
 }
+
+/* int main (void)
+{
+    printf("%s", (char *)ft_calloc(4, 3));
+    return (0);
+} */
